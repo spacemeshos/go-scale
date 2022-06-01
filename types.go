ec1edc30b723bf5b3508256d475dbfc933820f63
Dmitry <yashulyak@gmail.com>
2022-06-01 11:45:15 +0300

    add U8 typ

--- types.go
+++ types.go
@@ -82,3 +82,18 @@ type String = ByteSlice
 func (s String) String() string {
 	return string(s)
 }
+
+type U8 uint8
+
+func (u *U8) EncodeScale(e *Encoder) (int, error) {
+	return EncodeCompact8(e, uint8(*u))
+}
+
+func (u *U8) DecodeScale(d *Decoder) (int, error) {
+	r, n, err := DecodeCompact8(d)
+	if err != nil {
+		return n, err
+	}
+	*u = U8(r)
+	return n, nil
+}
