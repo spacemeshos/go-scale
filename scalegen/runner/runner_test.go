54a8ec7b94ec55f5184ac6ed2c702b3cb09dbbc6
Eugene Toropov <eugene.toropov@gmail.com>
2022-09-05 19:09:03 +0400

    added TestCleanupScaleFile

--- scalegen/runner/runner_test.go
+++ scalegen/runner/runner_test.go
@@ -1,6 +1,7 @@
 package runner
 
 import (
+	"io/ioutil"
 	"os"
 	"path/filepath"
 	"strings"
@@ -40,6 +41,51 @@ func TestGoldenExamples(t *testing.T) {
 			goldendata, err := os.ReadFile(golden)
 			require.NoError(t, err)
 			require.Equal(t, string(goldendata), string(outdata))
+		})
+	}
+}
+
+func testDataDir(tb testing.TB) string {
+	tb.Helper()
+	rel, err := filepath.Abs("./testdata")
+	require.NoError(tb, err)
+	return rel
+}
+
+func TestCleanupScaleFile(t *testing.T) {
+	dir := testDataDir(t)
+	files, err := os.ReadDir(dir)
+	require.NoError(t, err)
+
+	for _, file := range files {
+		file := file
+		if file.IsDir() {
+			continue
+		}
+		if strings.Contains(file.Name(), scaleSuffix) {
+			continue
+		}
+		t.Run(file.Name(), func(t *testing.T) {
+			scaleFile := filepath.Join(dir, ScaleFile(file.Name()))
+			scaleEmptyFile := scaleFile + ".empty"
+
+			scaleFileData, err := os.ReadFile(scaleFile)
+			require.NoError(t, err)
+			scaleEmptyFileData, err := os.ReadFile(scaleEmptyFile)
+			require.NoError(t, err)
+
+			scaleFileCopy := scaleFile + ".copy"
+			require.NoError(t, err)
+			err = ioutil.WriteFile(scaleFileCopy, scaleFileData, 0644)
+			defer os.Remove(scaleFileCopy)
+
+			err = cleanupScaleFile(scaleFileCopy)
+			require.NoError(t, err)
+
+			scaleFileCopyData, err := os.ReadFile(scaleFileCopy)
+			require.NoError(t, err)
+
+			require.Equal(t, string(scaleFileCopyData), string(scaleEmptyFileData))
 		})
 	}
 }
