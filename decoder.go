d1122a4ad455cd38cc8b59f0a8a00aca00368b47
Eugene Toropov <eugene.toropov@gmail.com>
2022-09-15 12:11:38 +0300

    update string slice decoding

--- decoder.go
+++ decoder.go
@@ -356,14 +356,10 @@ func DecodeStringSliceWithLimit(d *Decoder, limit uint32) ([]string, int, error)
 	}
 	result := make([]string, 0, len(sliceOfByteSlices))
 	for i := range sliceOfByteSlices {
-		result = append(result, byteSliceToString(sliceOfByteSlices[i]))
+		result = append(result, *(*string)(unsafe.Pointer(&sliceOfByteSlices[i])))
 	}
 
 	return result, n, nil
-}
-
-func byteSliceToString(bs []byte) string {
-	return *(*string)(unsafe.Pointer(&bs))
 }
 
 func DecodeStructArray[V any, H DecodablePtr[V]](d *Decoder, value []V) (int, error) {
