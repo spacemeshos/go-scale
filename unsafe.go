36500a7016c59904f58024f133b7dce8b22c114e
Matthias Fasching <5011972+fasmat@users.noreply.github.com>
2022-12-14 11:50:08 +0100

    Small improvements to unsafe operations (#39)

--- unsafe.go
+++ unsafe.go
@@ -8,24 +8,17 @@ import (
 // stringToBytes converts a string to a byte slice without copying the underlying data.
 // IMPORTANT: The returned byte slice must not be modified!
 // This is a low-level function and should be used carefully.
-func stringToBytes(s string) []byte {
-	if s == "" {
-		return nil
-	}
-
-	const max = 0x7fff0000
-	if len(s) > max {
-		panic("string too long")
-	}
-	return (*[max]byte)(unsafe.Pointer((*reflect.StringHeader)(unsafe.Pointer(&s)).Data))[:len(s):len(s)]
+func stringToBytes(s string) (b []byte) {
+	bh := (*reflect.SliceHeader)(unsafe.Pointer(&b))
+	sh := (*reflect.StringHeader)(unsafe.Pointer(&s))
+	bh.Data = sh.Data
+	bh.Cap = sh.Len
+	bh.Len = sh.Len
+	return b
 }
 
 // bytesToString converts a byte slice to a string without copying the underlying data.
 // This is a low-level function and should be used carefully.
 func bytesToString(b []byte) string {
-	if len(b) == 0 {
-		return ""
-	}
-
 	return *(*string)(unsafe.Pointer(&b))
 }
