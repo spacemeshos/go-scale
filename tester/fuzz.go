843115ab1528171a5e71603de48617f1e41a1d3e
Matthias Fasching <5011972+fasmat@users.noreply.github.com>
2023-05-15 22:25:46 +0200

    Fuzz consistency must also check number of bytes consumed (#56)
    * Fuzz consistency must also check number of bytes consumed
    
    * Check if encode returns the correct number of bytes encoded

--- tester/fuzz.go
+++ tester/fuzz.go
@@ -21,16 +21,19 @@ func FuzzConsistency[T any, H scale.TypePtr[T]](f *testing.F, fuzzFuncs ...any) 
 
 		buf := bytes.NewBuffer(nil)
 		enc := scale.NewEncoder(buf)
-		_, err := H(&object).EncodeScale(enc)
+		n1, err := H(&object).EncodeScale(enc)
 		if errors.Is(err, scale.ErrEncodeTooManyElements) {
 			return
 		}
 		require.NoError(t, err)
+		require.Equal(t, n1, buf.Len())
 
 		dec := scale.NewDecoder(buf)
 		var decoded T
-		_, err = H(&decoded).DecodeScale(dec)
+		n2, err := H(&decoded).DecodeScale(dec)
 		require.NoError(t, err)
+
+		require.Equal(t, n1, n2)
 
 		if !cmp.Equal(object, decoded, cmpopts.EquateEmpty()) {
 			t.Errorf("decoded didn't match original: %s", cmp.Diff(object, decoded))
