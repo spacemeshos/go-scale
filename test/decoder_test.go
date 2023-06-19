3dd35a8f52649b9338636ba2310a2d4ded0b9327
Matthias <5011972+fasmat@users.noreply.github.com>
2023-06-19 17:37:52 +0000

    Fix linter complaints

--- test/decoder_test.go
+++ test/decoder_test.go
@@ -4,8 +4,9 @@ import (
 	"bytes"
 	"testing"
 
-	"github.com/spacemeshos/go-scale"
 	"github.com/stretchr/testify/require"
+
+	"github.com/spacemeshos/go-scale"
 )
 
 func Test_DecodeDeepNestedModule(t *testing.T) {
