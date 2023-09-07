5dda31be7aa063b7e6d97c5b404e3abff900dd37
Matthias Fasching <5011972+fasmat@users.noreply.github.com>
2023-09-07 11:27:22 +0200

    Update to go 1.20 (#70)

--- go.mod
+++ go.mod
@@ -1,6 +1,6 @@
 module github.com/spacemeshos/go-scale
 
-go 1.19
+go 1.20
 
 require (
 	github.com/google/go-cmp v0.5.9
--- unsafe.go
+++ unsafe.go
@@ -1,7 +1,6 @@
 package scale
 
 import (
-	"reflect"
 	"unsafe"
 )
 
@@ -9,16 +8,17 @@ import (
 // IMPORTANT: The returned byte slice must not be modified!
 // This is a low-level function and should be used carefully.
 func stringToBytes(s string) (b []byte) {
-	bh := (*reflect.SliceHeader)(unsafe.Pointer(&b))
-	sh := (*reflect.StringHeader)(unsafe.Pointer(&s))
-	bh.Data = sh.Data
-	bh.Cap = sh.Len
-	bh.Len = sh.Len
-	return b
+	if s == "" {
+		return nil
+	}
+	return unsafe.Slice(unsafe.StringData(s), len(s))
 }
 
 // bytesToString converts a byte slice to a string without copying the underlying data.
 // This is a low-level function and should be used carefully.
 func bytesToString(b []byte) string {
-	return *(*string)(unsafe.Pointer(&b))
+	if len(b) == 0 {
+		return ""
+	}
+	return unsafe.String(unsafe.SliceData(b), len(b))
 }
