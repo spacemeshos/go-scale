e5030be8d0f5922317dce293ab11050409c33d14
Ivan Shvedunov <ivan4th@gmail.com>
2024-03-06 14:55:03 +0400

    Use max value constants in LenSize

--- encoder.go
+++ encoder.go
@@ -315,11 +315,11 @@ func EncodeStruct[V any, H EncodablePtr[V]](e *Encoder, value V) (int, error) {
 // LenSize returns the size in bytes required to encode a length value.
 func LenSize(v uint32) uint32 {
 	switch {
-	case v <= (1<<6 - 1):
+	case v <= maxUint6:
 		return 1
-	case v <= (1<<14 - 1):
+	case v <= maxUint14:
 		return 2
-	case v <= (1<<30 - 1):
+	case v <= maxUint30:
 		return 4
 	default:
 		return 5
